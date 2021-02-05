e9a76a7647b5758ed900711f6003671a4a337014
Ikem Nomso <ikemnomso@gmail.com>
2021-02-05 22:26:35 +0100

    styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import './Home.css';
-import { CSSTransition } from 'react-transition-group';
-import { Waypoint } from 'react-waypoint';
 
 const Home = () => {
-	const [point, setPoint] = React.useState(false);
-
-	const handleWaypointEnter = () => {
-		setPoint(true);
-	};
-
 	return (
 		<div className='home'>
 			<div className='home__banner'>
@@ -29,37 +21,34 @@ const Home = () => {
 					</button>
 				</div>
 			</div>
-			<Waypoint onEnter={handleWaypointEnter} bottomOffset='36%' />
-			<CSSTransition classNames='company' timeout={700} in={point}>
-				<div className='home__about container-fluid p-4 my-3 text-center text-lg-left d-flex flex-wrap flex-lg-nowrap justify-content-between align-items-center'>
-					<div className='info container-md'>
-						<p>
-							Lance events was established in 2005 by real estate entrepreneur
-							Donatus Enekwene, who has a passion for creating beautiful spaces
-							with the idea of bringing people together, for business,
-							celebration or the arts.
-						</p>
-						<p>
-							Since our establishment, we have hosted countless events from
-							Meetings & Conferences to Product launches to Screenings to
-							Private Celebrations and has gained a reputation for its
-							commitment to excellence, outstanding service and uncompromising
-							quality. From sophisticated intimate celebrations to elegant
-							affairs, we are dedicated to facilitating exceptional events.
-						</p>
-						<p>
-							With all the talent and creativity that is continually poured into
-							Lance events, it is diverse and ever-evolving. Our staff all work
-							together to make our venues one of the most stunning and beautiful
-							places to host your events. We are very hardworking and always
-							have our clients needs in mind whenever we do business.
-						</p>
-					</div>
-					<div className='image container-md'>
-						<img src='/images/fireworks.jpg' alt='fireworks' />
-					</div>
+			<div className='home__about container-fluid p-4 my-3 text-center text-lg-left d-flex flex-wrap flex-lg-nowrap justify-content-between align-items-center'>
+				<div className='info container-md'>
+					<p>
+						Lance events was established in 2005 by real estate entrepreneur
+						Donatus Enekwene, who has a passion for creating beautiful spaces
+						with the idea of bringing people together, for business, celebration
+						or the arts.
+					</p>
+					<p>
+						Since our establishment, we have hosted countless events from
+						Meetings & Conferences to Product launches to Screenings to Private
+						Celebrations and has gained a reputation for its commitment to
+						excellence, outstanding service and uncompromising quality. From
+						sophisticated intimate celebrations to elegant affairs, we are
+						dedicated to facilitating exceptional events.
+					</p>
+					<p>
+						With all the talent and creativity that is continually poured into
+						Lance events, it is diverse and ever-evolving. Our staff all work
+						together to make our venues one of the most stunning and beautiful
+						places to host your events. We are very hardworking and always have
+						our clients needs in mind whenever we do business.
+					</p>
 				</div>
-			</CSSTransition>
+				<div className='image container-md mt-3 mt-md-0'>
+					<img src='/images/fireworks.jpg' alt='fireworks' />
+				</div>
+			</div>
 
 			<div className='home__clients my-5'>
 				<h2 className='text-center text-uppercase'>Partners</h2>
